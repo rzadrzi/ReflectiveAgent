c03ad0e84231861e062bf33494db6d437c201493
Reza Darzi <darzi.reza@gmail.com>
2026-05-27 19:05:47 +0330

    feat: Add logger for project

---
+++ reflective_agent/utils/logging.py
@@ -0,0 +1,16 @@
+import logging
+
+def setup_logging(name:str)-> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s: %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
