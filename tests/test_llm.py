0193075b0ad5077d341c075a0969cf0ff9d9b8c1
Reza Darzi <darzi.reza@gmail.com>
2026-06-11 20:01:05 +0330

    test: Add test for llm

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -1,5 +1,21 @@
-from llm.prompt_builder import PromptBuilder
+# from llm.prompt_builder import PromptBuilder
+from click import prompt
+
+from reflective_agent.llm.prompt_builder import PromptBuilder
+from reflective_agent.llm.client import LLMClient
+from reflective_agent.utils.helpers import load_yaml
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+PROMPT_FILE = BASE_DIR / "config" / "prompts.yaml"
+prompts = load_yaml(str(PROMPT_FILE))
+
+def llm_prompt_builder():
+    print(prompts)
+    # builder = PromptBuilder()
 
 
-def test_llm_prompt_builder():
-    builder = PromptBuilder()
+if __name__ == "__main__":
+    llm_prompt_builder()
