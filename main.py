c9069d4c533d205276727fa472f17543fa518af5
Reza Darzi <darzi.reza@gmail.com>
2026-07-11 21:55:23 +0330

    chore

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
-    print("Hello from reflectiveagent!")
+    from reflective_agent.utils.helpers import load_yaml
+
+    from pathlib import Path
+
+    BASE_DIR = Path(__file__).resolve().parent
+
+    PROMPT_FILE = BASE_DIR / "config" / "prompts.yaml"
+    prompts = load_yaml(str(PROMPT_FILE))
 
 
 if __name__ == "__main__":
